d2e4fd16ee257cd7a336d1aa85595bb63ca5702e
Refinaldii <refinaldi9786@gmail.com>
2024-04-04 11:40:28 +0700

    menambahkan pilihan lanjutan pada akhir pencarian

--- AlgorithmaSearch_0200.cpp
+++ AlgorithmaSearch_0200.cpp
@@ -49,6 +49,8 @@ void LinearSearch()
 		if (i == n)
 			cout << "\n" << item << " not found in the array\n";
 		cout << "\nNumber of comparisons: " << ctr << endl;
+		cout << "\nCountinue search (y/n): ";
+		cin >> ch;
 	}
 }
 	
